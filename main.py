f94144725c1a3fccb7a9505a22fe4b241fcf0cf9
Anthony Audrain <anthonyaud78@gmail.com>
2022-09-05 14:21:20 +0200

    suppression de tinydb et Query

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from models import Tournaments
 from models import Rounds
 from models import Matchs
 from view import View
-from tinydb import TinyDB
-from tinydb import Query
-controller = Controller(Players, Tournaments, Rounds, Matchs, View, TinyDB, Query)
+
+controller = Controller(Players, Tournaments, Rounds, Matchs, View)
 controller.run()
