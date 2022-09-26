506282840a4cbdd0f9518256723968359c82c4f6
Anthony Audrain <anthonyaud78@gmail.com>
2022-09-26 16:34:40 +0200

    ajout des importations de plusieurs vues

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from controller import ControllerGen
 from models import Players, Tournaments, Rounds, Matchs
-from view import View
+from view import Views, ViewPromptPlayer, ViewPromptTournament, ViewPromptRound, ViewPhrasing
 from rich.console import Console
 from rich.table import Table
 from rich import print
@@ -8,5 +8,6 @@ from rich.tree import Tree
 from rich.prompt import Prompt
 from tinydb import TinyDB, Query
 
-controller = ControllerGen(Players, Tournaments, Rounds, Matchs, View, Console, Table, print, Tree, Prompt, TinyDB, Query)
+views = Views(ViewPromptPlayer, ViewPromptTournament, ViewPromptRound, ViewPhrasing)
+controller = ControllerGen(Players, Tournaments, Rounds, Matchs, views, Console, Table, print, Tree, Prompt, TinyDB, Query)
 controller.run()
