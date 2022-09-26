559b205e09b67f2f003d28a98480d674d9c3d4c1
Anthony Audrain <anthonyaud78@gmail.com>
2022-09-26 16:35:41 +0200

    ajout de plusieurs classes pour simplifier le code

--- view.py
+++ view.py
@@ -3,11 +3,12 @@ from rich.console import Console
 from rich import print
 from rich.prompt import Prompt
 from rich.table import Table
+
 verif_first_name = "^[A-Za-z]+$"
 verif_date = "^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}$"
 
 
-class View:
+class ViewPromptPlayer:
     @staticmethod
     def prompt_userName_player(reset_first_name):
         first_name = Prompt.ask("[bold blue]Saisisez le prenom du joueur")
@@ -53,6 +54,8 @@ class View:
         else:
             return ranking
 
+
+class ViewPromptTournament:
     @staticmethod
     def prompt_name_tournament(reset_name):
         name = Prompt.ask("[bold blue]Saisisez le nom du tournoi")
@@ -81,22 +84,6 @@ class View:
             return reset_date(reset_date)
 
     @staticmethod
-    def prompt_add_player(query, players, add_player, return_menu, menu, cr_tr):
-        player = query()
-        if len(players.all()) < 8:
-            return_menu(menu, cr_tr)
-        else:
-            while len(add_player) < 8:
-                first_name = Prompt.ask(f"[bold blue]Entrez le prenom du joueur{len(add_player) +1}")
-                name = Prompt.ask(f"[bold blue]Entrez le nom du joueur{len(add_player) +1}")
-                recupFirst_name = players.search(player.prenom == first_name)
-                recup_name = players.search(player.nom == name)
-                if recupFirst_name and recup_name:
-                    add_player.append(recupFirst_name[0])
-                else:
-                    print("[bold red]Joueur introuvable")
-
-    @staticmethod
     def prompt_time_tournament(reset_time):
         time = Prompt.ask("[bold blue]Saisisez le temps du tournoi(bullet,blitz,coup-rapide)")
         if time == "bullet" or time == "Bullet" or time == "BULLET":
@@ -109,20 +96,174 @@ class View:
             print("[bold red]Information incorrect")
             return reset_time(reset_time)
 
+
+class ViewPromptRound:
     @staticmethod
-    def promptEditRank(query, players, return_menu, menu_players, editPlayers):
-        test = query()
+    def prompt_heure_start_round():
+        heure_start = Prompt.ask("[bold blue]Entrez l'heure du début du round: ")
+        return heure_start
+
+    @staticmethod
+    def prompt_date_start_round(reset_date):
+        date_start = Prompt.ask("[bold blue]Entrez la date de début du round: ")
+        if re.match(verif_date, date_start):
+            return date_start
+        else:
+            print("[bold red]Information incorrect")
+            return reset_date(reset_date)
+
+    @staticmethod
+    def prompt_round():
+        round = Prompt.ask("[bold blue]Entrez le nom du round: ")
+        return round
+
+    @staticmethod
+    def prompt_nRound():
+        print("Créer le round")
+        nRound = int(Prompt.ask("[bold blue]Tapez le n° du round: "))
+        return nRound
+
+    @staticmethod
+    def prompt_heure_end_round():
+        heure_end = Prompt.ask("[bold blue]Entrez l'heure de fin du round: ")
+        return heure_end
+
+    @staticmethod
+    def prompt_date_end_round(reset_date):
+        date_end = Prompt.ask("[bold blue]Entrez la date de fin du round: ")
+        if re.match(verif_date, date_end):
+            return date_end
+        else:
+            print("[bold red]Information incorrect")
+            return reset_date(reset_date)
+
+
+class ViewPhrasing:
+    @staticmethod
+    def phrasing_create_tournament():
+        print("[bold green]Tournoi créer")
+
+    @staticmethod
+    def phrasing_create_player():
+        print("[bold green]Joueur créer")
+
+    @staticmethod
+    def phrasing_none_players():
+        print("[bold red]Aucun joueurs enregistrés")
+
+    @staticmethod
+    def phrasing_none_tournaments():
+        print("[bold red]Il n'y a pas de tournois dans la base de donnée")
+
+    @staticmethod
+    def prompt_phrasing_name_tournament():
+        tournament = Prompt.ask("[bold blue]Tapez le nom du tournoi: ")
+        return tournament
+
+    @staticmethod
+    def phrasing_tournament():
+        print("[bold red]Tournoi introuvable")
+
+
+class Views:
+    def __init__(self, promptPlayer, promptTournament, promptRounds, phrasing):
+        self.promptPlayer = promptPlayer
+        self.promptTournament = promptTournament
+        self.promptRounds = promptRounds
+        self.phrasing = phrasing
+
+    def prompt_userName_player(self):
+        return self.promptPlayer.prompt_userName_player(self.promptPlayer.prompt_userName_player)
+
+    def prompt_name_player(self):
+        return self.promptPlayer.prompt_name_player(self.promptPlayer.prompt_name_player)
+
+    def prompt_dateBirth_player(self):
+        return self.promptPlayer.prompt_dateBirth_player(self.promptPlayer.prompt_dateBirth_player)
+
+    def prompt_sex_player(self):
+        return self.promptPlayer.prompt_sex_player(self.promptPlayer.prompt_sex_player)
+
+    def prompt_ranking_player(self):
+        return self.promptPlayer.prompt_ranking_player()
+
+    def prompt_name_tournament(self):
+        return self.promptTournament.prompt_name_tournament(self.promptTournament.prompt_name_tournament)
+
+    def prompt_lieu_tournament(self):
+        return self.promptTournament.prompt_lieu_tournament(self.promptTournament.prompt_lieu_tournament)
+
+    def prompt_date_tournament(self):
+        return self.promptTournament.prompt_date_tournament(self.promptTournament.prompt_date_tournament)
+
+    def prompt_time_tournament(self):
+        return self.promptTournament.prompt_time_tournament(self.promptTournament.prompt_time_tournament)
+
+    def prompt_heure_start_round(self):
+        return self.promptRounds.prompt_heure_start_round()
+
+    def prompt_heure_end_round(self):
+        return self.promptRounds.prompt_heure_end_round()
+
+    def prompt_date_start_round(self):
+        return self.promptRounds.prompt_date_start_round(self.promptRounds.prompt_date_start_round)
+
+    def prompt_date_end_round(self):
+        return self.promptRounds.prompt_date_end_round(self.promptRounds.prompt_date_end_round)
+
+    def prompt_round(self):
+        return self.promptRounds.prompt_round()
+
+    def prompt_nRound(self):
+        return self.promptRounds.prompt_nround()
+
+    def phrasing_create_tournament(self):
+        self.phrasing.phrasing_create_tournament()
+
+    def phrasing_create_player(self):
+        self.phrasing.phrasing_create_player()
+
+    def phrasing_none_players(self):
+        self.phrasing.phrasing_none_players()
+
+    def phrasing_none_tournaments(self):
+        self.phrasing.phrasing_none_tournaments()
+
+    def prompt_phrasing_name_tournament(self):
+        self.phrasing.prompt_phrasing_name_tournament()
+
+    def phrasing_tournament(self):
+        self.phrasing.phrasing_tournament()
+
+    @staticmethod
+    def prompt_add_player(query, players, add_player, return_menu, menu, cr_tr):
+        player = query()
+        if len(players.all()) < 8:
+            return_menu(menu, cr_tr)
+        else:
+            while len(add_player) < 8:
+                first_name = Prompt.ask(f"[bold blue]Entrez le prenom du joueur{len(add_player) + 1}")
+                name = Prompt.ask(f"[bold blue]Entrez le nom du joueur{len(add_player) + 1}")
+                recupFirst_name = players.search(player.prenom == first_name)
+                recup_name = players.search(player.nom == name)
+                if recupFirst_name and recup_name:
+                    add_player.append(recupFirst_name[0])
+                else:
+                    print("[bold red]Joueur introuvable")
+
+    @staticmethod
+    def promptEditRank(query, players):
+        verif = query()
+        print(players.all())
         first_name = Prompt.ask("[bold blue]Saissisez le prenom du joueur")
         name = Prompt.ask("[bold blue]Saissisez le nom du joueur")
-        if players.search(test.prenom == first_name) and players.search(test.nom == name):
+        if players.search(verif.prenom == first_name) and players.search(verif.nom == name):
             rank = int(Prompt.ask(f"[bold blue]Rentrer le nouveau classement de {first_name} {name}"))
-            players.update({"classement": rank}, test.prenom == first_name)
+            players.update({"classement": rank}, verif.prenom == first_name)
             print("[bold green]Classement mis à jour")
-            return_menu(menu_players, editPlayers)
 
         else:
             print("[bold red]Joueur introuvable")
-            return_menu(menu_players, editPlayers)
 
     @staticmethod
     def menu(menu1, menu2, menu3):
@@ -147,7 +288,7 @@ class View:
             exit()
         else:
             print("[bold red]Information incorrect")
-            View.menu(menu1, menu2, menu3)
+            Views.menu(menu1, menu2, menu3)
 
     @staticmethod
     def menu_player(menu1, menu2, menu3, menu4):
@@ -161,7 +302,7 @@ class View:
         console = Console()
         console.print(table)
 
-        question = int(Prompt.ask("[bold blue]Saisisez 1, 2, 3: "))
+        question = int(Prompt.ask("[bold blue]Saisisez 1, 2, 3, 4: "))
         if question == 1:
             menu1()
         elif question == 2:
@@ -172,10 +313,10 @@ class View:
             menu4()
         else:
             print("[bold red]Information incorrect")
-            View.menu_player(menu1, menu2, menu3)
+            Views.menu_player(menu1, menu2, menu3, menu4)
 
     @staticmethod
-    def menu_tournament(menu1, menu2, menu3, menu4, ):
+    def menu_tournament(menu1, menu2, menu3, menu4):
 
         table = Table()
         table.add_column("MENU TOURNOI", justify="center", style="cyan", no_wrap=True)
@@ -197,28 +338,7 @@ class View:
             menu4()
         else:
             print("[bold red]Informations incorrect")
-            View.menu_tournament(menu1, menu2, menu3, menu4)
-
-    @staticmethod
-    def get_players_tournaments_database(get_players_tournament_database):
-        print("[bold blue]Joueurs enregistré: ")
-        get_players_tournament_database()
-
-    @staticmethod
-    def phrasing_create_tournament():
-        print("[bold green]Tournoi créer")
-
-    @staticmethod
-    def phrasing_create_player():
-        print("[bold green]Joueur créer")
-
-    @staticmethod
-    def phrasing_none_players():
-        print("[bold red]Aucun joueurs enregistrés")
-
-    @staticmethod
-    def phrasing_none_tournaments():
-        print("[bold red]Il n'y a pas de tournois dans la base de donnée")
+            Views.menu_tournament(menu1, menu2, menu3, menu4)
 
     @staticmethod
     def return_menu(menu, choice_menu):
@@ -229,7 +349,7 @@ class View:
             choice_menu()
         else:
             print("[bold red]Informations incorrect")
-            View.return_menu(menu, choice_menu)
+            Views.return_menu(menu, choice_menu)
 
     @staticmethod
     def phrasing_len_players(players_table_all):
@@ -245,50 +365,6 @@ class View:
             print(f"[bold red]Il y a {len(players_table_all())} deja enregistrés")
 
     @staticmethod
-    def prompt_round():
-        round = Prompt.ask("[bold blue]Entrez le nom du round: ")
-        return round
-
-    @staticmethod
-    def prompt_phrasing_name_tournament():
-        tournament = Prompt.ask("[bold blue]Tapez le nom du tournoi: ")
-        return tournament
-
-    @staticmethod
-    def prompt_heure_start_round():
-        heure_start = Prompt.ask("[bold blue]Entrez l'heure du début du round: ")
-        return heure_start
-
-    @staticmethod
-    def prompt_date_start_round(reset_date):
-        date_start = Prompt.ask("[bold blue]Entrez la date de début du round: ")
-        if re.match(verif_date, date_start):
-            return date_start
-        else:
-            print("[bold red]Information incorrect")
-            return reset_date(reset_date)
-
-    @staticmethod
-    def prompt_heure_end_round():
-        heure_end = Prompt.ask("[bold blue]Entrez l'heure de fin du round: ")
-        return heure_end
-
-    @staticmethod
-    def prompt_date_end_round(reset_date):
-        date_end = Prompt.ask("[bold blue]Entrez la date de fin du round: ")
-        if re.match(verif_date, date_end):
-            return date_end
-        else:
-            print("[bold red]Information incorrect")
-            return reset_date(reset_date)
-
-    @staticmethod
-    def prompt_nRound():
-        print("Créer le round")
-        nRound = int(Prompt.ask("[bold blue]Tapez le n° du round: "))
-        return nRound
-
-    @staticmethod
     def printNRound(number_round):
         if number_round == 0:
             print("Il n'y a pas de round créer")
@@ -299,6 +375,4 @@ class View:
         else:
             print("Il y a deja plusieurs rounds qui ont été créer")
 
-    @staticmethod
-    def phrasing_tournament():
-        print("[bold red]Tournoi introuvable")
+
