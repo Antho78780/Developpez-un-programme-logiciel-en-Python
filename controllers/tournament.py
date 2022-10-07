7753625479e58963aca425f46bb0f16fad98b9c2
Anthony Audrain <anthonyaud78@gmail.com>
2022-10-07 16:20:45 +0200

    ajout du controlleur indépendant tournois

--- controllers/tournament.py
+++ controllers/tournament.py
@@ -1,20 +1,3 @@
-from models.player import Players
-from models.round import Rounds
-from models.match import Matchs
-
-from views.player import PromptPlayer
-from views.tournament import PromptTournament
-from views.round import PromptRound
-
-from tinydb import TinyDB, Query
-from rich.console import Console
-from rich.table import Table
-from rich.prompt import Prompt
-
-from controllers import player
-from controllers import round
-
-
 class Tournament:
     def __init__(self, modelTournament, modelRounds, modelMatchs, viewTournament, viewPlayer, viewRound,
                  tinyDB, query, table, console, tree, print, prompt):
@@ -64,7 +47,9 @@ class Tournament:
         self.viewTournament.return_menu(self.menu_tournament, self.create_tournament)
 
     def get_players_tournament_database(self):
-        comePlayer = player.Player(Players, PromptPlayer, TinyDB, Query, Table, Console)
+        import main
+        from controllers.player import Player
+        comePlayer = Player(main.Players, main.PromptPlayer, main.TinyDB, main.Query, main.Table, main.Console)
         if not self.players_table.all() == []:
             comePlayer.display_style_players_database(self.players_table.all())
             self.viewPlayer.phrasing_len_players(self.players_table.all)
@@ -73,44 +58,48 @@ class Tournament:
 
     def rapports(self):
         self.viewTournament.phrasingRapport(self.tournaments_table)
-        for i in self.tournaments_table.all():
-            i["joueurs"].sort(key=lambda x: (x["classement"], x["prenom"]))
-            rounds = i["rounds"]
-            name = i["nom"]
-            lieu = i["lieu"]
-            date = i["date"]
-            time = i["temps"]
-            number_rounds = i["number_round"]
-            joueurs = i["joueurs"]
-            joueurs.sort(key=lambda x: (x.get("prenom"), x.get("classement")))
+        try:
+            for i in self.tournaments_table.all():
+                i["joueurs"].sort(key=lambda x: (x["classement"], x["prenom"]))
+                rounds = i["rounds"]
+                name = i["nom"]
+                lieu = i["lieu"]
+                date = i["date"]
+                time = i["temps"]
+                number_rounds = i["number_round"]
+                joueurs = i["joueurs"]
+                joueurs.sort(key=lambda x: (x.get("prenom"), x.get("classement")))
 
-            table_tournaments = self.table()
-            table_players = self.table()
-            table_rounds = self.table()
-            table_tournaments.add_column("Nom du Tournoi", justify="center", style="cyan", no_wrap=True)
-            table_tournaments.add_column("Lieu", justify="center", style="cyan", no_wrap=True)
-            table_tournaments.add_column("Date", justify="center", style="cyan", no_wrap=True)
-            table_tournaments.add_column("Temps", justify="center", style="cyan", no_wrap=True)
-            table_tournaments.add_column("Nombre de rounds", justify="center", style="cyan", no_wrap=True)
-            table_tournaments.add_row(name, lieu, date, time, str(number_rounds))
+                table_tournaments = self.table()
+                table_players = self.table()
+                table_rounds = self.table()
+                table_tournaments.add_column("Nom du Tournoi", justify="center", style="cyan", no_wrap=True)
+                table_tournaments.add_column("Lieu", justify="center", style="cyan", no_wrap=True)
+                table_tournaments.add_column("Date", justify="center", style="cyan", no_wrap=True)
+                table_tournaments.add_column("Temps", justify="center", style="cyan", no_wrap=True)
+                table_tournaments.add_column("Nombre de rounds", justify="center", style="cyan", no_wrap=True)
+                table_tournaments.add_row(name, lieu, date, time, str(number_rounds))
 
-            table_players.add_column("Joueurs", justify="center", style="cyan", no_wrap=True)
-            table_players.add_column("Classement", justify="center", style="cyan", no_wrap=True)
-            for a in joueurs:
-                table_players.add_row(a["prenom"], str(a["classement"]))
+                table_players.add_column("Joueurs", justify="center", style="cyan", no_wrap=True)
+                table_players.add_column("Classement", justify="center", style="cyan", no_wrap=True)
+                for a in joueurs:
+                    table_players.add_row(a["prenom"], str(a["classement"]))
 
-            table_rounds.add_column("Match", justify="center", style="cyan", no_wrap=True)
-            numero_round = 0
-            for a in rounds[3], rounds[9], rounds[15], rounds[21]:
-                numero_round += 1
-                table_rounds.add_row(f"[bold green]Round {numero_round}:[/] {a[0][0][0]} vs {a[0][1][0]},"
-                                     f" {a[1][0][0]} vs {a[1][1][0]}, {a[2][0][0]} vs {a[2][1][0]},"
-                                     f" {a[3][0][0]} vs {a[3][1][0]}")
-            arbre_tournaments = self.tree("Tournoi")
-            arbre_tournaments.add(table_tournaments)
-            arbre_tournaments.add(table_players)
-            arbre_tournaments.add(table_rounds)
-            self.print(arbre_tournaments)
+                table_rounds.add_column("Match", justify="center", style="cyan", no_wrap=True)
+                numero_round = 0
+                for a in rounds[3], rounds[9], rounds[15], rounds[21]:
+                    numero_round += 1
+                    table_rounds.add_row(f"[bold green]Round {numero_round}:[/] {a[0][0][0]} vs {a[0][1][0]},"
+                                         f" {a[1][0][0]} vs {a[1][1][0]}, {a[2][0][0]} vs {a[2][1][0]},"
+                                         f" {a[3][0][0]} vs {a[3][1][0]}")
+                arbre_tournaments = self.tree("Tournoi")
+                arbre_tournaments.add(table_tournaments)
+                arbre_tournaments.add(table_players)
+                arbre_tournaments.add(table_rounds)
+                self.print(arbre_tournaments)
+        except IndexError:
+            self.viewTournament.phrasing_error2()
+            self.viewTournament.return_menu(self.menu_tournament, self.rapports)
 
     def get_tournaments_database(self):
         if not self.tournaments_table.all() == []:
@@ -140,13 +129,18 @@ class Tournament:
             self.viewTournament.return_menu(self.menu_tournament, self.get_tournaments_database)
 
     def menu_tournament(self):
-        comeRound = round.Round(Rounds, Matchs, PromptPlayer, PromptTournament,
-                                PromptRound, TinyDB, Query, Console, Table, self.print, Prompt)
+        import main
+        from controllers.round import Round
+        comeRound = Round(main.Rounds, main.Matchs, main.PromptPlayer, main.PromptTournament, main.PromptRound,
+                          main.TinyDB, main.Query, main.Console, main.Table, main.print, main.Prompt)
+
         self.viewTournament.menu_tournament(self.create_tournament, comeRound.create_round,
                                             self.get_tournaments_database, self.menu)
 
     def menu(self):
-        comePlayer = player.Player(Players, PromptPlayer, TinyDB, Query, Table, Console)
+        import main
+        from controllers.player import Player
+        comePlayer = Player(main.Players, main.PromptPlayer, main.TinyDB, main.Query, main.Table, main.Console)
         comePlayer.menu()
 
 
