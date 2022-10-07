fb9ac5c4d99c517c838e7d41c2688c951fc9220e
Anthony Audrain <anthonyaud78@gmail.com>
2022-10-07 16:16:37 +0200

    simplification de packages

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from controllers.player import Player
+from controllers import tournament
 
+from models.tournament import Tournaments
+from models.round import Rounds
+from models.match import Matchs
 from models.player import Players
 
+from views.tournament import PromptTournament
 from views.player import PromptPlayer
-
-from rich.console import Console
-from rich.table import Table
+from views.round import PromptRound
 
 from tinydb import TinyDB, Query
 
+from rich.table import Table
+from rich.console import Console
+from rich.tree import Tree
+from rich import print
+from rich.prompt import Prompt
+
+from controllers.player import Player
 controllerPlayer = Player(Players, PromptPlayer, TinyDB, Query, Table, Console)
 controllerPlayer.menu()
